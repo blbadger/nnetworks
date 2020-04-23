1e1256a3304bbad8802c792bb59932316147d158
Benjamin Badger <54602201+blbadger@users.noreply.github.com>
2020-04-22 21:39:57 -0400

    Update NN_prep_snippets.py

--- NN_prep_snippets.py
+++ NN_prep_snippets.py
@@ -1,7 +1,14 @@
+	
+# Image slicer- preps images for classification via NNs
+from PIL import Image
+import image_slicer
 
+for i in range(1, 30):
+	im = Image.open('/Users/badgerbl/Desktop/Control/Control_{}.jpg'.format(i))
+	image_slicer.slice('/Users/badgerbl/Desktop/Control/Control_{}.jpg'.format(i), 16)
+	
 
 # renames files with a name that is easily indexed for future iteration
-
 import os
 
 path = '/home/bbadger/Desktop/GFP_2/snap29'
@@ -9,3 +16,13 @@ files = os.listdir(path)
 
 for index, file in enumerate(files):
 	os.rename(os.path.join(path, file), os.path.join(path, ''.join(['s', str(index), '.png'])))
+
+	
+#Image rotater- preps images for classification via NNs by rotation
+from PIL import Image
+
+for i in range(1, 348):
+	im = Image.open('/Users/badgerbl/Desktop/unrotated/Control_{}.png'.format(i))
+	for j in range(-5,5):
+		im.rotate(j)
+		im.save('/Users/badgerbl/Desktop/Control_rotated/Control_{}_{}.png'.format(i, j))

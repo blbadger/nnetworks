d33c988dc195f3d83389b9b60ca635dac0357b2b
bbadger <bbadger10@gmail.com>
2021-05-07 19:58:52 -0400

    updated fmnist_bench

--- fmnist_bench.py
+++ fmnist_bench.py
@@ -1,12 +1,9 @@
 """
-Tensorflow_sequential_deep.py
+fmnist_bench.py
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Implementation of a Keras sequential neural network using a 
-Tensorflow backend, combined with modules to display pre- classified
-images before the network trains as well as a subset of test images
-with classification and % confidence for each image.  The latter script
-is optimized for binary classification but can be modified for more than
-two classes.
+Implementation of convolutional neural nets using Keras front-end and a
+Tensorflow backend.  Slightly modified versions of Deep_network.py and 
+AlexNet.py architectures are employed to classify fashion MNIST images.
 """
 
 ### Libraries
@@ -20,6 +17,8 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import regularizers
+from tensorflow.keras import layers
 from PIL import Image 
 import numpy as np 
 import matplotlib.pyplot as plt 
@@ -30,7 +29,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(tf.__version__)
+# Preprocess data
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 
@@ -38,6 +37,7 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 			   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
@@ -49,11 +49,127 @@ train_image_number = len(train_images)
 train_images = train_images.reshape(train_image_number, img_dimensions[0], img_dimensions[1], 1)
 test_images = test_images.reshape(test_image_number, img_dimensions[0], img_dimensions[1], 1)
 
-# model = tf.keras.Sequential([
-#     tf.keras.layers.Flatten(input_shape=(28, 28)),
-#     tf.keras.layers.Dense(128, activation='relu'),
-#     tf.keras.layers.Dense(10)
-# ])
+
+def superdeep_network(train_images, train_labels, test_images, test_labels):
+	### Neural network model: specifies the architecture using the Sequential Keras model:
+	### Conv2D(16, 3, padding='same', activation='relu' signifies a convolutional layers
+	### of 16 filters, a local receptive field of 3x3, padding such that the output dimensions
+	### are equal to input dimension (ie 256x256 input --> 256x256 output), and activation
+	### function for the layer)
+	model = tf.keras.models.Sequential([
+		Conv2D(16, (3, 3), 
+			padding='same', 
+			activation='relu', 
+			input_shape=(28, 28, 1), 
+			data_format='channels_last'),
+		MaxPooling2D(),
+		Conv2D(16, 3, 
+			padding='same', 
+			activation='relu'),
+		MaxPooling2D(),
+		Conv2D(16, 3, 
+			padding='same', 
+			activation='relu'),
+		MaxPooling2D(),
+		Conv2D(32, 3, 
+			padding='same', 
+			activation='relu'),
+		MaxPooling2D(),
+		Conv2D(32, 3, 
+			padding='same', 
+			activation='relu'),
+		Conv2D(32, 3, 
+			padding='same', 
+			activation='relu'),
+		# MaxPooling2D(),
+		Conv2D(64, 3, 
+			padding='same', 
+			activation='relu'),
+		# MaxPooling2D(),
+		Conv2D(64, 3, 
+			padding='same', 
+			activation='relu'),
+		# MaxPooling2D(),
+		Conv2D(64, 3, 
+			padding='same', 
+			activation='relu'),
+		# MaxPooling2D(),
+		Flatten(),
+		Dense(512, activation='relu'),
+		Dense(50, activation='relu'),
+		Dense(10, activation='softmax')
+	])
+
+	model.compile(optimizer='Adam', 
+		loss = 'sparse_categorical_crossentropy', # labels are integers
+		metrics=['accuracy'])
+
+	model.summary()
+
+	model.fit(train_images, train_labels, epochs=20, batch_size = 20, verbose=1)
+
+	test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+
+def alexnet(train_images, train_labels, test_images, test_labels):
+	# AlexNet model
+
+	model = tf.keras.models.Sequential([
+	    Conv2D(96, (11, 11), 
+	    	activation='relu', 
+	    	input_shape=(28, 28, 1), 
+	    	strides=(4,4), 
+	    	kernel_regularizer=regularizers.l2(0.0005)),
+	    MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+	    layers.BatchNormalization(),
+	    Conv2D(256, 5, 
+	    	padding='same', 
+	    	activation='relu', 
+	    	kernel_regularizer=regularizers.l2(0.0005)),
+	    # MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+	    layers.BatchNormalization(),
+	    Conv2D(384, 3, 
+	    	padding='same', 
+	    	activation='relu', 
+	    	kernel_regularizer=regularizers.l2(0.0005)),
+	    Conv2D(384, 3, 
+	    	padding='same', 
+	    	activation='relu', 
+	    	kernel_regularizer=regularizers.l2(0.0005)),
+	    Conv2D(256, 3, 
+	    	padding='same', 
+	    	activation='relu', 
+	    	kernel_regularizer=regularizers.l2(0.0005)),
+	    # MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+	    Flatten(),
+	    Dropout(0.5),
+	    Dense(2048, 
+	    	activation='relu', 
+	    	kernel_regularizer=regularizers.l2(0.0005)),
+	    Dropout(0.5),
+	    Dense(2048, 
+	    	activation='relu', 
+	    	kernel_regularizer=regularizers.l2(0.0005)),
+	    Dense(10, 
+	    	activation='softmax', 
+	    	kernel_regularizer=regularizers.l2(0.0005))
+	])
+
+	### Compile model: choose loss and optimization functions
+
+	model.compile(optimizer=tf.keras.optimizers.SGD(momentum=0.9, lr=0.01, decay=0.3), 
+		loss = 'sparse_categorical_crossentropy', # labels are integers
+		metrics=['accuracy'])
+
+	model.summary()
+
+	model.fit(train_images, train_labels, epochs=20, batch_size = 20)
+
+	test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+superdeep_network(train_images, train_labels, test_images, test_labels)
+
+alexnet(train_images, train_labels, test_images, test_labels)
 
 '''
 ##################################
@@ -83,58 +199,6 @@ show_batch(image_batch, label_batch)
 ###################################
 '''
 
-
-### Neural network model: specifies the architecture using the Sequential Keras model:
-### Conv2D(16, 3, padding='same', activation='relu' signifies a convolutional layers
-### of 16 filters, a local receptive field of 3x3, padding such that the output dimensions
-### are equal to input dimension (ie 256x256 input --> 256x256 output), and activation
-### function for the layer)
-
-model = tf.keras.models.Sequential([
-	Conv2D(16, (3, 3), padding='same', activation='relu', input_shape=(28, 28, 1), data_format='channels_last'),
-	MaxPooling2D(),
-	Conv2D(16, 3, padding='same', activation='relu'),
-	MaxPooling2D(),
-	Conv2D(16, 3, padding='same', activation='relu'),
-	MaxPooling2D(),
-	Conv2D(32, 3, padding='same', activation='relu'),
-	MaxPooling2D(),
-	Conv2D(32, 3, padding='same', activation='relu'),
-	Conv2D(32, 3, padding='same', activation='relu'),
-	# MaxPooling2D(),
-	Conv2D(64, 3, padding='same', activation='relu'),
-	# MaxPooling2D(),
-	Conv2D(64, 3, padding='same', activation='relu'),
-	# MaxPooling2D(),
-	Conv2D(64, 3, padding='same', activation='relu'),
-	# MaxPooling2D(),
-	Flatten(),
-	Dense(512, activation='relu'),
-	Dense(50, activation='relu'),
-	Dense(10, activation='softmax')
-])
-
-model.compile(optimizer='Adam', 
-	loss = 'sparse_categorical_crossentropy', # labels are integers
-	metrics=['accuracy'])
-
-model.summary()
-
-# model.compile(optimizer='adam',
-#               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-#               metrics=['accuracy'])
-
-model.fit(train_images, train_labels, epochs=10, batch_size = 20)
-
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
-
-print('\nTest accuracy:', test_acc)
-
-predictions = model.predict(test_images)
-
-
-
-
 ### Creates a panel of images classified by the trained neural network.
 
 # image_batch, label_batch = next(test_data_gen1)
@@ -162,18 +226,17 @@ predictions = model.predict(test_images)
 # 		'Snf7' if prediction[0]>=0.5 else 'Control', 
 # 		'Snf7' if true_label[0]==1. else 'Control'), color = color)
 
+# 	num_rows = 4
+# 	num_cols = 3
+# 	num_images = 24
 
-# num_rows = 4
-# num_cols = 3
-# num_images = 24
+# 	plt.figure(figsize = (num_rows, num_cols))
 
-# plt.figure(figsize = (num_rows, num_cols))
+# 	for i in range(num_images):
+# 	  plt.subplot(num_rows, 2*num_cols, i+1)
+# 	  plot_image(i+1, predictions, test_labels, test_images)
 
-# for i in range(num_images):
-#   plt.subplot(num_rows, 2*num_cols, i+1)
-#   plot_image(i+1, predictions, test_labels, test_images)
-
-# plt.show() 
+# 	plt.show() 
 
 
 
